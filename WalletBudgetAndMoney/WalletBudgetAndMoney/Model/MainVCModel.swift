b3a2f92d7f676a53026f7a02a87c7fa7d8624b45
tan1dan <ivandeveloper4@gmail.com>
2024-05-03 13:21:20 +0200

    Transactions

---
+++ WalletBudgetAndMoney/WalletBudgetAndMoney/Model/MainVCModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainVCModel.swift
+//  WalletBudgetAndMoney
+//
+//  Created by Иван Знак on 03/05/2024.
+//
+
+import Foundation
--- WalletBudgetAndMoney/WalletBudgetAndMoney/Resources+Help/CellItem.swift
+++ WalletBudgetAndMoney/WalletBudgetAndMoney/Resources+Help/CellItem.swift
@@ -18,6 +18,18 @@ struct AccountItem: Hashable {
     var name: String?
     var amount: Double?
     var currency: String?
+    var transactions: [Transaction]?
+}
+
+struct Transaction: Hashable {
+    var id: String
+    var account: AccountItem
+    var name: String
+    var type: Types
+    var subType: SubTypes
+    var category: String
+    var date: Date
+    var amount: Double
 }
 
 struct DataItem: Hashable {
