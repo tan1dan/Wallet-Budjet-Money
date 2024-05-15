615847cedd3238cfd60fd3ae0d3260c38bccb237
tan1dan <ivandeveloper4@gmail.com>
2024-05-15 17:09:32 +0200

    Models

--- WalletBudgetAndMoney/WalletBudgetAndMoney/Model/GraphDataModel.swift
+++ WalletBudgetAndMoney/WalletBudgetAndMoney/Model/GraphDataModel.swift
@@ -1,10 +1,3 @@
-//
-//  DataModel.swift
-//  SwiftGraphKit_Example
-//
-//  Created by Charles Bessonnet on 25/12/2018.
-//  Copyright © 2018 CocoaPods. All rights reserved.
-//
 
 import UIKit
 
@@ -29,169 +22,165 @@ class GraphDataModel {
         case .week:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 7 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
                 
             }
         case .month:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count - 1{
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 30 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
             
         case .threeMonth:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 90 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
             
         case .halfYear:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 180 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
             reports.reverse()
         case .year:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 365 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
             reports.reverse()
         }
-        
-//        let date = Date()
-//        let max  = Int(dataFrame.width)
-//        
-//        let minAmount = Float(dataFrame.minY)
-//        let maxAmount = Float(dataFrame.maxY)
-//        
-//        for i in 0..<max {
-//            let amount = CGFloat(Float.random(in: minAmount..<maxAmount))
-//            let day: TimeInterval = 24 * 60 * 60
-//            let currentDate = date.addingTimeInterval(TimeInterval(i - max) * day)
-//            let report = DailyReport(index: i, date: currentDate, amount: amount)
-//            
-//            reports.append(report)
-//        }
-        
     }
     
     func updateData(){
+        reports = []
         switch time {
         case .week:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+    
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 7 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
             
         case .month:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count - 1{
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 30 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
         case .threeMonth:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 90 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
             
         case .halfYear:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 180 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
         case .year:
             let accounts = Model.shared.accounts
             var transactions: [Transaction] = []
-            for i in 0...accounts.count {
-                transactions += accounts[i].transactions!
+            for account in accounts {
+                transactions += account.transactions!
             }
             transactions.sort(by: {$0.date > $1.date})
             var filteredTransactions = transactions.filter({$0.date <= Date() && $0.date >= Date() - 60 * 60 * 24 * 365 })
             var index = 0
             filteredTransactions.reverse()
             for transaction in filteredTransactions {
-                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(transaction.restAmount)))
+                let amountRounding = Double(round(100 * transaction.restAmount) / 100)
+                reports.append(DailyReport(index: index, date: transaction.date, amount: CGFloat(amountRounding)))
                 index += 1
             }
         }
--- WalletBudgetAndMoney/WalletBudgetAndMoney/Model/MainVCModel.swift
+++ WalletBudgetAndMoney/WalletBudgetAndMoney/Model/MainVCModel.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class Model {
+    
     static let shared = Model()
-    var accounts: [AccountItem] = [AccountItem(id: UUID().uuidString, image: UIImage(resource: .income), name: "Santander", amount: Double("1000.00") ?? (0), currency: "PLN", transactions: [Transaction(id: UUID().uuidString, name: "Начальные деньги", type: .income, category: .income, date: Date(), amount: Double("1000.00") ?? 0, restAmount: 1000.00)])]
+    var accounts: [AccountItem] = []
+    
+    private init() {
+        accounts = UserDefaultsManager.shared.read(key: UserDefaultsManager.key) ?? []
+    }
 }
 
